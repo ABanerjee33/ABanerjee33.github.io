ce0c8c53f1c82747315de09b34410e020abbe433
Avik Banerjee <staurus2013@gmail.com>
2023-08-11 01:26:10 +0000

    Deploy new build

---
+++ precache-manifest.a647f0937806d2e6df17263109fa191b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbe7f183dc3eb47ccc19415ec7c8e94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eb65875785cb0ec717e",
+    "url": "/static/css/3.e3af89ee.chunk.css"
+  },
+  {
+    "revision": "fb156a33b95cbd2f24da",
+    "url": "/static/css/4.b01e2f60.chunk.css"
+  },
+  {
+    "revision": "78f15d4b2aca37e1a343",
+    "url": "/static/css/main.48ec7c1e.chunk.css"
+  },
+  {
+    "revision": "094c0966a211eb48cd6e",
+    "url": "/static/js/2.7551389e.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.7551389e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb65875785cb0ec717e",
+    "url": "/static/js/3.2694d0f6.chunk.js"
+  },
+  {
+    "revision": "fb156a33b95cbd2f24da",
+    "url": "/static/js/4.7e77a6e9.chunk.js"
+  },
+  {
+    "revision": "78f15d4b2aca37e1a343",
+    "url": "/static/js/main.e00afc70.chunk.js"
+  },
+  {
+    "revision": "7d40cf6a9c8c0f276458",
+    "url": "/static/js/runtime-main.f4df6a6d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "94d10760099736e3976b3aca3d160cca",
+    "url": "/static/media/BWSICubeSat.94d10760.jpg"
+  },
+  {
+    "revision": "501d331db80ddc4600f0a32209f52497",
+    "url": "/static/media/Elephant1.501d331d.png"
+  },
+  {
+    "revision": "894d239834a062f2a724050d936e1d98",
+    "url": "/static/media/Elephant2.894d2398.png"
+  },
+  {
+    "revision": "d01131d310f027c9f04708a1c370317c",
+    "url": "/static/media/Elephant3.d01131d3.png"
+  },
+  {
+    "revision": "ac70dfcf487d9a5f9db826332c4bbc4f",
+    "url": "/static/media/GTLogoBetter.ac70dfcf.png"
+  },
+  {
+    "revision": "7c37cdee75581f0e37e55028140ddb31",
+    "url": "/static/media/IEEE.7c37cdee.png"
+  },
+  {
+    "revision": "b730019246ffea5bce3b746eaf512f42",
+    "url": "/static/media/LLBetter.b7300192.jpg"
+  },
+  {
+    "revision": "f9ed832684a8759eb0cf91ebd5e1355e",
+    "url": "/static/media/Linkedin.f9ed8326.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
