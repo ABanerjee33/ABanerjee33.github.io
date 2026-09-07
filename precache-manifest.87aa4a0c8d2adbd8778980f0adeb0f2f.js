11511ea6852dd758ab6da8489b2bbf5d1c06199f
Avik Banerjee <info@skyfallspace.com>
2026-09-07 02:17:13 -0700

    Deploy redesigned portfolio and Mission Lab

---
+++ precache-manifest.87aa4a0c8d2adbd8778980f0adeb0f2f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402a44befd9587510a7e6b693d561c99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f1f98354bdba8617e0f",
+    "url": "/static/css/3.b2327ecf.chunk.css"
+  },
+  {
+    "revision": "ded0d6f913302d7c8e14",
+    "url": "/static/css/4.79b055f5.chunk.css"
+  },
+  {
+    "revision": "e2de212b00c06c934636",
+    "url": "/static/css/main.97b838d8.chunk.css"
+  },
+  {
+    "revision": "c77e76066ddb75964d42",
+    "url": "/static/js/2.a22a0dde.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.a22a0dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1f98354bdba8617e0f",
+    "url": "/static/js/3.4f3f2c00.chunk.js"
+  },
+  {
+    "revision": "ded0d6f913302d7c8e14",
+    "url": "/static/js/4.dbe604e4.chunk.js"
+  },
+  {
+    "revision": "e2de212b00c06c934636",
+    "url": "/static/js/main.c58674fd.chunk.js"
+  },
+  {
+    "revision": "1cb6a9c9f436dc58c5ed",
+    "url": "/static/js/runtime-main.898dc6f1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "94d10760099736e3976b3aca3d160cca",
+    "url": "/static/media/BWSICubeSat.94d10760.jpg"
+  },
+  {
+    "revision": "501d331db80ddc4600f0a32209f52497",
+    "url": "/static/media/Elephant1.501d331d.png"
+  },
+  {
+    "revision": "894d239834a062f2a724050d936e1d98",
+    "url": "/static/media/Elephant2.894d2398.png"
+  },
+  {
+    "revision": "d01131d310f027c9f04708a1c370317c",
+    "url": "/static/media/Elephant3.d01131d3.png"
+  },
+  {
+    "revision": "7e43317bd6c11889b2bf0e44b14f96c7",
+    "url": "/static/media/GTVertical_TechGold.7e43317b.png"
+  },
+  {
+    "revision": "7c37cdee75581f0e37e55028140ddb31",
+    "url": "/static/media/IEEE.7c37cdee.png"
+  },
+  {
+    "revision": "b730019246ffea5bce3b746eaf512f42",
+    "url": "/static/media/LLBetter.b7300192.jpg"
+  },
+  {
+    "revision": "f9ed832684a8759eb0cf91ebd5e1355e",
+    "url": "/static/media/Linkedin.f9ed8326.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "50b3c5df9c3b853021437a5748be2332",
+    "url": "/static/media/skyfall-space-logo.50b3c5df.svg"
+  },
+  {
+    "revision": "f2b1b847235119d0ae094a606c74fca4",
+    "url": "/static/media/spacex.f2b1b847.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
